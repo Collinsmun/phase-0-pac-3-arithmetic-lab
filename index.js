5659ea91b1bc3bc462da3f42676b6bfeda3dc615
Collins Mundia <collins.mwangi@student.moringaschool.com>
2022-06-03 06:48:00 +0300

    assignment

---
+++ index.js
@@ -0,0 +1,43 @@
+function add(a,b){
+    return a+b;
+
+}
+
+function subtract(a,b){
+    return a-b;
+
+}
+
+function multiply(a,b){
+    return a*b;
+
+}
+function divide(a,b){
+    return a/b;
+
+}
+//  let n=4;
+function increment(n){
+    return (n+=1);
+
+}
+increment(n);
+
+function decrement(n){
+    return (n -= 1);
+
+}
+
+// let string=4.33
+function makeInt(string){
+    return `${string}`;
+     parseInt("string",10);
+
+}
+console.log(parseInt(4.33))
+
+function preserveDecimal(string){
+    return  `${string};`
+    parseFloat 
+
+}
